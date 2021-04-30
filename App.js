f703ec04c9b57fda42acbd6b12ae7737e285f970
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-04-30 16:05:01 -0400

    input validation

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react'
-import  uuid  from 'uuid';
+import React, { useState } from 'react'
+import uuid from 'uuid';
 
-import {View, Text, Image, StyleSheet, FlatList} from 'react-native';
+import { View, Text, Image, StyleSheet, FlatList, Alert } from 'react-native';
 import Header from './components/Header'
 import AddItem from './components/AddItem'
 import ListItem from './components/ListItem'
 
 const App = () => {
 
-  const [ items, setItems] = useState([
-    {id:uuid(), text:'Milk' },
-    {id:uuid(), text:'Eggs' },
-    {id:uuid(), text:'Bread' },
-    {id:uuid(), text:'Juice' },
+  const [items, setItems] = useState([
+    { id: uuid(), text: 'Milk' },
+    { id: uuid(), text: 'Eggs' },
+    { id: uuid(), text: 'Bread' },
+    { id: uuid(), text: 'Juice' },
   ]);
 
   const deleteItem = (id) => {
@@ -21,26 +21,35 @@ const App = () => {
     });
   };
 
-  const addItem = (text) => {
-    setItems(prevItems =>{
-      return [{id: uuid(), text}, ...prevItems];
-    });
-  };
+  const addItem = text => {
+    if (!text) {
+      Alert.alert(
+        "Error",
+        `Must Enter An Item`,  
+          [{text: "OK"}]
+      );
+    } else {
+      setItems(prevItems => {
+        return [{ id: uuid(), text }, ...prevItems];
+      });
+    }
+  }; 
 
   return (
-      <View style = {styles.container}>
-        <Header title = 'Shopping List'/>
-        <AddItem addItem = {addItem}/>
-        <FlatList data = {items} 
-        renderItem = {({item}) => <ListItem deleteItem = {deleteItem} item = {item}/>}
-        />
-        </View>
+    <View style={styles.container}>
+      <Header title='Shopping List' />
+      <AddItem addItem={addItem} />
+      <FlatList data={items}
+        renderItem={({ item }) => <ListItem deleteItem={deleteItem} item={item} />}
+      />
+
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1, 
+  container: {
+    flex: 1,
   }
 })
 
--- components/AddItem.js
+++ components/AddItem.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
-import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity,} from 'react-native';
 import {Icon} from 'react-native-elements'
 
 
 
-const AddItem = ({title,addItem}) => {
+const AddItem = ({title, addItem}) => {
 
-const [text, setText] = useState('');
+const [text, setText] = useState();
 
 const onChange = (textValue) => setText(textValue);
 
